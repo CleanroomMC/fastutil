041c800e13cb566625542bc27ff4b08e06e7fbcb
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-01-13 01:58:17 +0800

    Expand predefined templating symbol table

--- template/constants.ts
+++ template/constants.ts
@@ -1,5 +1,15 @@
-export const templatingTypes = ['boolean'];
+export const templatingTypes = ['boolean', 'byte', 'short', 'int', 'long', 'float', 'double'] as const;
 export type TemplatingTypes = typeof templatingTypes[number];
+
+export const wrapperTypes: {[key in TemplatingTypes]: string} = {
+    'boolean': 'Boolean',
+    'byte': 'Byte',
+    'short': 'Short',
+    'int': 'Integer',
+    'long': 'Long',
+    'float': 'Float',
+    'double': 'Double'
+};
 
 export type TemplatingProps = {
     [key: string]: string
@@ -11,12 +21,18 @@ export const commonTemplate: TemplatingProps = {
     packagePath: 'com.cleanroommc.fastutil'
 };
 
+function capitalize(literal: string): string {
+    return literal[0].toUpperCase() + literal.slice(1);
+}
+
 export function genTypedTemplate(type: TemplatingTypes, classNameTemplating: TypeTemplating): TemplatingProps {
     let typedTemplate = commonTemplate;
 
     typedTemplate['typePackagePath'] = `${typedTemplate['packagePath']}.${type}`;
     typedTemplate['typeName'] = type;
+    typedTemplate['capitalizedTypeName'] = capitalize(type);
     typedTemplate['className'] = classNameTemplating(typedTemplate);
+    typedTemplate['wrapperClassName'] = wrapperTypes[type];
 
     return typedTemplate;
 }
