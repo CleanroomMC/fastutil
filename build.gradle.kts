7361126ae34491eccd2da2477d9851714c96ea61
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-01-15 01:07:43 +0800

    Add fastutil as dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,8 @@ repositories {
 }
 
 dependencies {
+    implementation("it.unimi.dsi:fastutil:8.5.11")
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
