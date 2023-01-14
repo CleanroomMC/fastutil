f1bcfec83469787bdaaecc2f6915ba57033a71c9
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-01-15 01:11:10 +0800

    Comment refactor

--- template/templates/ConcurrentArrayList.tmpl.java
+++ template/templates/ConcurrentArrayList.tmpl.java
@@ -63,15 +63,15 @@ public class {{ className }} extends Abstract{{ capitalizedPrimitiveTypeName }}L
     protected int size;
     /** Ensures that the component type of the given array is the proper type.
 
-      * This is irrelevant for primitive types, so it will just do a trivial copy.
+     * This is irrelevant for primitive types, so it will just do a trivial copy.
 
-      * But for Reference types, you can have a {@code String[]} masquerading as an {@code Object[]},
+     * But for Reference types, you can have a {@code String[]} masquerading as an {@code Object[]},
 
-      * which is a case we need to prepare for because we let the user give an array to use directly
+     * which is a case we need to prepare for because we let the user give an array to use directly
 
-      * with {@link #wrap}.
+     * with {@link #wrap}.
 
-      */
+     */
 
     private static final {{ primitiveTypeName }}[] copyArraySafe({{ primitiveTypeName }}[] a, int length) {
     	if (length == 0) return {{ capitalizedPrimitiveTypeName }}Arrays.EMPTY_ARRAY;
@@ -84,15 +84,15 @@ public class {{ className }} extends Abstract{{ capitalizedPrimitiveTypeName }}L
 
     /** Creates a new array list using a given array.
 
-      *
+     *
 
-      * <p>This constructor is only meant to be used by the wrapping methods.
+     * <p>This constructor is only meant to be used by the wrapping methods.
 
-      *
+     *
 
-      * @param a the array that will be used to back this array list.
+     * @param a the array that will be used to back this array list.
 
-      */
+     */
     protected {{ className }}(final {{ primitiveTypeName }} a[], @SuppressWarnings("unused") boolean wrapped) {
     	this.a = a;
     }
@@ -105,11 +105,11 @@ public class {{ className }} extends Abstract{{ capitalizedPrimitiveTypeName }}L
 
     /** Creates a new array list with given capacity.
 
-      *
+     *
 
-      * @param capacity the initial capacity of the array list (may be 0).
+     * @param capacity the initial capacity of the array list (may be 0).
 
-      */
+     */
 	public {{ className }}(final int capacity) {
 	    initArrayFromCapacity(capacity);
 	}
@@ -121,11 +121,11 @@ public class {{ className }} extends Abstract{{ capitalizedPrimitiveTypeName }}L
 
     /** Creates a new array list and fills it with a given collection.
 
-      *
+     *
 
-      * @param c a collection that will be used to fill the array list.
+     * @param c a collection that will be used to fill the array list.
 
-      */
+     */
 	public {{ className }}(final Collection<? extends {{ wrapperClassName }}> c) {
         if (c instanceof {{ capitalizedPrimitiveTypeName }}ArrayList) {
             a = copyArrayFromSafe(({{ capitalizedPrimitiveTypeName }}ArrayList) c);
@@ -142,11 +142,11 @@ public class {{ className }} extends Abstract{{ capitalizedPrimitiveTypeName }}L
 
     /** Creates a new array list and fills it with a given type-specific collection.
 
-      *
+     *
 
-      * @param c a type-specific collection that will be used to fill the array list.
+     * @param c a type-specific collection that will be used to fill the array list.
 
-      */
+     */
 	public {{ className }}(final {{ capitalizedPrimitiveTypeName }}Collection c) {
 	    if (c instanceof {{ capitalizedPrimitiveTypeName }}ArrayList) {
 	        a = copyArrayFromSafe(({{ capitalizedPrimitiveTypeName }}ArrayList) c);
@@ -163,11 +163,11 @@ public class {{ className }} extends Abstract{{ capitalizedPrimitiveTypeName }}L
 
     /** Creates a new array list and fills it with a given type-specific list.
 
-      *
+     *
 
-      * @param l a type-specific list that will be used to fill the array list.
+     * @param l a type-specific list that will be used to fill the array list.
 
-      */
+     */
 	public {{ className }}(final {{ capitalizedPrimitiveTypeName }}List l) {
 	    if (l instanceof {{ capitalizedPrimitiveTypeName }}ArrayList) {
 	        a = copyArrayFromSafe(({{ capitalizedPrimitiveTypeName }}ArrayList) l);
@@ -180,26 +180,26 @@ public class {{ className }} extends Abstract{{ capitalizedPrimitiveTypeName }}L
 
     /** Creates a new array list and fills it with the elements of a given array.
 
-      *
+     *
 
-      * @param a an array whose elements will be used to fill the array list.
+     * @param a an array whose elements will be used to fill the array list.
 
-      */
+     */
 	public {{ className }}(final {{ primitiveTypeName }} a[]) {
 	    this(a, 0, a.length);
 	}
 
     /** Creates a new array list and fills it with the elements of a given array.
 
-      *
+     *
 
-      * @param a an array whose elements will be used to fill the array list.
+     * @param a an array whose elements will be used to fill the array list.
 
-      * @param offset the first element to use.
+     * @param offset the first element to use.
 
-      * @param length the number of elements to use.
+     * @param length the number of elements to use.
 
-      */
+     */
 	public {{ className }}(final {{ primitiveTypeName }} a[], final int offset, final int length) {
          this(length);
          System.arraycopy(a, offset, this.a, 0, length);
@@ -208,11 +208,11 @@ public class {{ className }} extends Abstract{{ capitalizedPrimitiveTypeName }}L
 
     /** Creates a new array list and fills it with the elements returned by an iterator..
 
-      *
+     *
 
-      * @param i an iterator whose returned elements will fill the array list.
+     * @param i an iterator whose returned elements will fill the array list.
 
-      */
+     */
 	public {{ className }}(final Iterator<? extends {{ wrapperClassName }}> i) {
          this();
          while(i.hasNext()) this.add((i.next()).{{ primitiveTypeName }}Value());
@@ -220,11 +220,11 @@ public class {{ className }} extends Abstract{{ capitalizedPrimitiveTypeName }}L
 
     /** Creates a new array list and fills it with the elements returned by a type-specific iterator..
 
-      *
+     *
 
-      * @param i a type-specific iterator whose returned elements will fill the array list.
+     * @param i a type-specific iterator whose returned elements will fill the array list.
 
-      */
+     */
 	public {{ className }}(final {{ capitalizedPrimitiveTypeName }}Iterator i) {
          this();
          while(i.hasNext()) this.add(i.next{{ wrapperClassName }}());
@@ -232,11 +232,11 @@ public class {{ className }} extends Abstract{{ capitalizedPrimitiveTypeName }}L
 
 	/** Returns the snapshot of backing array of this list.
 
-	  *
+     *
 
-	  * @return the backing array.
+     * @return the backing array.
 
-	  */
+     */
 	public {{ primitiveTypeName }}[] elements() {
 	    rlock.lock();
 	    try {
@@ -248,23 +248,23 @@ public class {{ className }} extends Abstract{{ capitalizedPrimitiveTypeName }}L
 
 	/** Wraps a given array into an array list of given size.
 
-	  *
+     *
 
-	  * <p>Note it is guaranteed
+     * <p>Note it is guaranteed
 
-	  * that the type of the array returned by {@link #elements()} will be the same
+     * that the type of the array returned by {@link #elements()} will be the same
 
-	  * (see the comments in the class documentation).
+     * (see the comments in the class documentation).
 
-	  *
+     *
 
-	  * @param a an array to wrap.
+     * @param a an array to wrap.
 
-	  * @param length the length of the resulting array list.
+     * @param length the length of the resulting array list.
 
-	  * @return a new array list of the given size, wrapping the given array.
+     * @return a new array list of the given size, wrapping the given array.
 
-	  */
+     */
 	public static {{ className }} wrap(final {{ primitiveTypeName }} a[], final int length) {
 	    if (length > a.length) throw new IllegalArgumentException("The specified length (" + length + ") is greater than the array size (" + a.length + ")");
 	    final {{ className }} l = new {{ className }}(a, true);
@@ -274,58 +274,58 @@ public class {{ className }} extends Abstract{{ capitalizedPrimitiveTypeName }}L
 
 	/** Wraps a given array into an array list.
 
-	  *
+     *
 
-	  * <p>Note it is guaranteed
+     * <p>Note it is guaranteed
 
-	  * that the type of the array returned by {@link #elements()} will be the same
+     * that the type of the array returned by {@link #elements()} will be the same
 
-	  * (see the comments in the class documentation).
+     * (see the comments in the class documentation).
 
-	  *
+     *
 
-	  * @param a an array to wrap.
+     * @param a an array to wrap.
 
-	  * @return a new array list wrapping the given array.
+     * @return a new array list wrapping the given array.
 
-	  */
+     */
 	public static {{ className }} wrap(final {{ primitiveTypeName }} a[]) {
 	    return wrap(a, a.length);
 	}
 
 	/** Creates a new empty array list.
 
-	  *
+     *
 
-	  * @return a new empty array list.
+     * @return a new empty array list.
 
-	  */
+     */
 	public static {{ className }} of() {
 	    return new {{ className }}();
 	}
 
 	/** Creates an array list using an array of elements.
 
-	  *
+     *
 
-	  * @param init a the array the will become the new backing array of the array list.
+     * @param init a the array the will become the new backing array of the array list.
 
-	  * @return a new array list backed by the given array.
+     * @return a new array list backed by the given array.
 
-	  * @see #wrap
+     * @see #wrap
 
-	  */
+     */
 	public static {{ className }} of(final {{ primitiveTypeName }}... init) {
 	    return wrap(init);
 	}
 
 	/** Ensures that this array list can contain the given number of entries without resizing.
 
-	  *
+	 *
 
-	  * @param capacity the new minimum capacity for this array list.
+	 * @param capacity the new minimum capacity for this array list.
 
-	  */
+     */
 	public void ensureCapacity(final int capacity) {
 	    wlock.lock();
 	    try {
@@ -344,6 +344,8 @@ public class {{ className }} extends Abstract{{ capitalizedPrimitiveTypeName }}L
 	  *
 
 	  * This method assumes the list is being locked by write lock.
+
+	  *
 
 	  * @param capacity the new minimum capacity for this array list.
 
@@ -566,4 +568,214 @@ public class {{ className }} extends Abstract{{ capitalizedPrimitiveTypeName }}L
 	        wlock.unlock();
 	    }
 	}
+
+    private class ConcurrentSubList extends AbstractConcurrent{{ capitalizedPrimitiveTypeName }}List.Concurrent{{ capitalizedPrimitiveTypeName }}RandomAccessSubList {
+		/** The locks. */
+		private final ReadWriteLock lock = new ReentrantReadWriteLock();
+		private final WriteLock wlock = lock.writeLock();
+		private final ReadLock rlock = lock.readLock();
+
+        protected ConcurrentSubList(int from, int to) {
+            super({{ className }}.this.elements(), from, to);
+        }
+
+        // Most of the inherited methods should be fine, but we can override a few of them for performance.
+        // Needed because we can't access the parent class' instance variables directly in a different instance of SubList.
+        private boolean[] getParentArray() {
+            rlock.lock();
+            try {
+                return a;
+            } finally {
+                rlock.unlock();
+            }
+        }
+
+        @Override
+        public {{ primitiveTypeName }} get{{ capitalizedPrimitiveTypeName }}(int i) {
+            rlock.lock();
+            try {
+                ensureRestrictedIndex(i);
+                return a[i + from];
+            } finally {
+                rlock.unlock();
+            }
+        }
+
+		private final class ConcurrentSubListIterator extends {{ captilizedPrimitiveTypeName }}Iterators.AbstractIndexBasedListIterator {
+            // We are using pos == 0 to be 0 relative to SubList.from (meaning you need to do a[from + i] when accessing array).
+            SubListIterator(int index) {
+                super(0, index);
+            }
+
+            @Override
+            protected boolean get(int i) {
+                return a[from + i];
+            }
+
+            @Override
+            protected void add(int i, boolean k) {
+				ConcurrentSubList.wlock.lock();
+				try {
+					ConcurrentSubList.this.add(i, k);
+				} finally {
+					ConcurrentSubList.this.wlock.unlock();
+				}
+            }
+
+            @Override
+            protected void set(int i, boolean k) {
+				ConcurrentSubList.wlock.lock();
+				try {
+					ConcurrentSubList.this.set(i, k);
+				} finally {
+					ConcurrentSubList.this.wlock.unlock();
+				}
+            }
+
+            @Override
+            protected void remove(int i) {
+                ConcurrentSubList.wlock.lock();
+				try {
+					ConcurrentSubList.this.remove(i);
+				} finally {
+					ConcurrentSubList.this.wlock.unlock();
+				}
+            }
+
+            @Override
+            protected int getMaxPos() {
+                return to - from;
+            }
+
+            @Override
+            public boolean nextBoolean() {
+                if (!hasNext()) throw new NoSuchElementException();
+                return a[from + (lastReturned = pos++)];
+            }
+
+            @Override
+            public boolean previousBoolean() {
+                if (!hasPrevious()) throw new NoSuchElementException();
+                return a[from + (lastReturned = --pos)];
+            }
+
+            @Override
+            public void forEachRemaining(final BooleanConsumer action) {
+                final int max = to - from;
+                while (pos < max) {
+                    action.accept(a[from + (lastReturned = pos++)]);
+                }
+            }
+        }
+
+        @Override
+        public BooleanListIterator listIterator(int index) {
+            return new SubListIterator(index);
+        }
+
+        private final class SubListSpliterator extends BooleanSpliterators.LateBindingSizeIndexBasedSpliterator {
+            // We are using pos == 0 to be 0 relative to real array 0
+            SubListSpliterator() {
+                super(from);
+            }
+
+            private SubListSpliterator(int pos, int maxPos) {
+                super(pos, maxPos);
+            }
+
+            @Override
+            protected int getMaxPosFromBackingStore() {
+                return to;
+            }
+
+            @Override
+            protected boolean get(int i) {
+                return a[i];
+            }
+
+            @Override
+            protected SubListSpliterator makeForSplit(int pos, int maxPos) {
+                return new SubListSpliterator(pos, maxPos);
+            }
+
+            @Override
+            public boolean tryAdvance(final BooleanConsumer action) {
+                if (pos >= getMaxPos()) return false;
+                action.accept(a[pos++]);
+                return true;
+            }
+
+            @Override
+            public void forEachRemaining(final BooleanConsumer action) {
+                final int max = getMaxPos();
+                while (pos < max) {
+                    action.accept(a[pos++]);
+                }
+            }
+        }
+
+        @Override
+        public BooleanSpliterator spliterator() {
+            return new SubListSpliterator();
+        }
+
+        boolean contentsEquals(boolean[] otherA, int otherAFrom, int otherATo) {
+            if (a == otherA && from == otherAFrom && to == otherATo) return true;
+            if (otherATo - otherAFrom != size()) {
+                return false;
+            }
+            int pos = from, otherPos = otherAFrom;
+            // We have already assured that the two ranges are the same size, so we only need to check one bound.
+            // TODO When minimum version of Java becomes Java 9, use the Arrays.equals which takes bounds, which is vectorized.
+            // Make sure to split out the reference equality case when you do this.
+            while (pos < to) if (a[pos++] != otherA[otherPos++]) return false;
+            return true;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == this) return true;
+            if (o == null) return false;
+            if (!(o instanceof java.util.List)) return false;
+            if (o instanceof BooleanArrayList other) {
+
+                return contentsEquals(other.a, 0, other.size());
+            }
+            if (o instanceof SubList other) {
+
+                return contentsEquals(other.getParentArray(), other.from, other.to);
+            }
+            return super.equals(o);
+        }
+
+        int contentsCompareTo(boolean[] otherA, int otherAFrom, int otherATo) {
+            if (a == otherA && from == otherAFrom && to == otherATo) return 0;
+            // TODO When minimum version of Java becomes Java 9, use Arrays.compare, which vectorizes.
+            boolean e1, e2;
+            int r, i, j;
+            for (i = from, j = otherAFrom; i < to && i < otherATo; i++, j++) {
+                e1 = a[i];
+                e2 = otherA[j];
+                if ((r = (Boolean.compare((e1), (e2)))) != 0) return r;
+            }
+            return i < otherATo ? -1 : (i < to ? 1 : 0);
+        }
+
+        @Override
+        public int compareTo(final java.util.List<? extends Boolean> l) {
+            if (l instanceof BooleanArrayList other) {
+
+                return contentsCompareTo(other.a, 0, other.size());
+            }
+            if (l instanceof SubList other) {
+
+                return contentsCompareTo(other.getParentArray(), other.from, other.to);
+            }
+            return super.compareTo(l);
+        }
+        // We don't override subList as we want AbstractList's "sub-sublist" nesting handling,
+        // which would be tricky to do here.
+        // TODO Do override it so array access isn't sent through N indirections.
+        // This will likely mean making this class static.
+    }
 }
